587ffd3338952f05b01fd0aa01654c6e5c892f2c
mrexodia <mr.exodia.tpodt@gmail.com>
2017-03-19 12:34:52 +0100

    implemented various functions in the pykd adapter

--- pykd.py
+++ pykd.py
@@ -1,5 +1,10 @@
 import platform
 import traceback
+import array
+import x64dbgpy.pluginsdk.x64dbg as x64dbg
+import x64dbgpy.pluginsdk._scriptapi as script
+
+version = '0.3.2.1'
 
 def notImplemented():
     try:
@@ -12,74 +17,100 @@ def is64bitSystem():
     return platform.architecture()[0] == '64bit'
 
 def dbgCommand(command, suppressOutput = False):
+    print "command: %s" % command
     notImplemented()
 
 def dprintln(str, dml = False):
-    notImplemented()
+    print str
 
 def findMemoryRegion(va):
-    notImplemented()
+    base = script.GetBase(va)
+    size = script.GetSize(va)
+    if base == 0 or size == 0:
+        raise MemoryException('No such address 0x%x' % va)
+    return base, size
 
 def findSymbol(va):
     notImplemented()
 
 def getCurrentProcess():
-    notImplemented()
+    return x64dbg.DbgGetPebAddress(x64dbg.DbgGetProcessId())
 
 def getCurrentProcessId():
-    notImplemented()
+    return x64dbg.DbgGetProcessId()
 
 def getImplicitThread():
-    # returns the current TEB address
-    notImplemented()
+    return x64dbg.DbgGetTebAddress(x64dbg.DbgGetThreadId())
 
 def getProcessThreads():
-    # returns a list of TEB addresses
-    notImplemented()
+    l = x64dbg.THREADLIST()
+    x64dbg.DbgGetThreadList(l)
+    result = []
+    if l.count > 0:
+        return [t.BasicInfo.ThreadLocalBase for t in x64dbg.GetThreadInfoList(l)]
+    return result
 
 def getVaProtect(va):
-    notImplemented()
+    return script.GetProtect(va)
 
 def isValid(va):
-    notImplemented()
+    return x64dbg.DbgMemIsValidReadPtr(va)
 
 def loadBytes(va, count):
-    notImplemented()
+    return list(script.Read(va, count))
 
 def loadChars(va, count):
-    traceback.print_stack()
+    return script.Read(va, count)
 
 def loadCStr(va):
-    notImplemented()
+    return loadChars(va, 256).rstrip('\0')
 
 def loadDwords(va, count):
-    notImplemented()
+    A = array.array('I')
+    A.fromstring(script.Read(va, count * 4))
+    return A.tolist()
 
 def loadUnicodeString(va):
-    # returns string inside UNICODE_STRING at va
-    notImplemented()
+    # http://www.nirsoft.net/kernel_struct/vista/UNICODE_STRING.html
+    Length = script.ReadWord(va)
+    va += 2
+    MaximumLength = script.ReadWord(va)
+    va += 2
+    if is64bitSystem():
+        va += 4
+    Buffer = script.ReadPtr(va)
+    if Length > MaximumLength or not script.IsValidPtr(Buffer):
+        raise DbgException('Corrupted UNICODE_STRING structure')
+    A = array.array('u')
+    A.fromstring(script.Read(Buffer, Length * 2))
+    return A.tounicode()
 
 def loadWChars(va, count):
-    notImplemented()
+    A = array.array('u')
+    A.fromstring(script.Read(va, count * 2))
+    return A.tounicode()
 
 def loadWStr(va):
-    notImplemented()
+    return loadWChars(va, 256 * 2).rstrip(u'\0')
 
 def ptrDWord(va):
-    notImplemented()
+    return script.ReadDword(va)
+
+def writeBytes(va, data):
+    A = array.array('B')
+    A.fromlist(data)
+    script.Write(va, A.tostring())
 
 def reg(name):
-    notImplemented()
+    return x64dbg.DbgValFromString(name)
+
+def ptrPtr(va):
+    return script.ReadPtr(va)
 
 def typedVar(type, va):
     notImplemented()
 
 def typedVarList(va, type, flink):
-    notImplemented()
-
-version = '0.3.2.1'
-
-def ptrPtr(va):
     notImplemented()
 
 class disasm:
@@ -176,7 +207,6 @@ class module:
         notImplemented()
 
     def getFixedFileInfo(self):
-
         notImplemented()
 
     def getVersion(self):
